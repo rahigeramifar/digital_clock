d51001630d1b39398051961903f8c169031000e4
rahigeramifar <38390267+rahigerami@users.noreply.github.com>
2019-09-09 01:12:40 -0500

    commit

--- digiclock.js
+++ digiclock.js
@@ -1,9 +1,9 @@
 function go()
 	{
-		var time = new Date();
-		var hour = time.getHours();
-		var minute = time.getMinutes();
-		var seconds = time.getSeconds();
+		var date = new Date();
+		var hour = date.getHours();
+		var minute = date.getMinutes();
+		var seconds = date.getSeconds();
 		var ampm = "AM";
 
 		if ( hour > 12)
@@ -11,16 +11,23 @@ function go()
 				hour = hour - 12;
 				ampm = "PM";
 			}
-
 		if (hour == 0)
 			{
-				hour = 12;
+			hour = 12;
 			}
-
 		if (hour < 10)
 			{
 			hour = "0" + hour;
 			}
-document.body.innerHTML= "<h1> The current time is "+ hour ":"+ minute +":" + seconds +"</h1>"
-		//document.body.innerHTML= "<h1> The date today is  "+ (date.getMonth()+1) + "/" + date.getDate() + "/" + date.getFullYear() +"</h1>"
+		if (minute < 10)
+			{
+			minute = "0" + hour;
+			}
+		if (seconds < 10)
+			{
+			seconds = "0" + seconds;
+			}
+
+document.body.innerHTML= "<h1> The current time is "+ hour +":"+ minute +":" + seconds +"</h1>"
+//document.body.innerHTML= "<h1> The date today is  "+ (date.getMonth()+1) + "/" + date.getDate() + "/" + date.getFullYear() +"</h1>"
 	}
