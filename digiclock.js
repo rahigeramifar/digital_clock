a957f79603091d10f4d9a34a1aeeb30fcbcd2f52
rahigeramifar <38390267+rahigerami@users.noreply.github.com>
2019-09-09 00:29:14 -0500

    am/pm clock

--- digiclock.js
+++ digiclock.js
@@ -1,5 +1,26 @@
-function go()
+function digitalClock()
 	{
-		var date = new Date()
-		document.body.innerHTML= "<h1> The date today is  "+ (date.getMonth()+1) + "/" + date.getDate() + "/" + date.getFullYear() +"</h1>"
+		var time = new Date();
+		var hour = time.getHours();
+		var minute = time.getMinutes();
+		var seconds = time.getSeconds();
+		var ampm = "AM";
+
+		if ( hour > 12)
+			{
+				hour = hour - 12;
+				ampm = "PM";
+			}
+
+		if (hour == 0)
+			{
+				hour = 12;
+			}
+
+		if (hour < 10)
+			{
+			hour = "0" + hour;
+			}
+
+		//document.body.innerHTML= "<h1> The date today is  "+ (date.getMonth()+1) + "/" + date.getDate() + "/" + date.getFullYear() +"</h1>"
 	}
