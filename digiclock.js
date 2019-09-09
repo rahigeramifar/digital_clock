8c9c166927b2b2433997ed5bba8447256b1964ce
rahigeramifar <38390267+rahigerami@users.noreply.github.com>
2019-09-08 21:13:54 -0500

    a

---
+++ digiclock.js
@@ -0,0 +1,10 @@
+function go()
+	{
+		var date = new Date()
+		document.body.innerHTML= "<h1> The date today is  "+ (date.getMonth()+1) + "/" + date.getDate() + "/" + date.getFullYear() +"</h1>"
+
+
+
+
+
+	}
